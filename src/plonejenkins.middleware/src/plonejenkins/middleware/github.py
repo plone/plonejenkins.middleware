47d4b2435c645271d89a6ec35db91022caa15818
Eric Steele <EricSteele47@gmail.com>
2013-02-09 15:33:06 +0100

    Basic Github wrapper.

---
+++ src/plonejenkins.middleware/src/plonejenkins/middleware/github.py
@@ -0,0 +1,36 @@
+from github import Github
+
+
+DEV_TEAM_ID = 14533
+
+
+class GithubConnection(object):
+
+    def __init__(self, username, password):
+        self.github = Github(username, password)
+
+    @property
+    def organization(self):
+        return self.github.get_organization('plone')
+
+    @property
+    def developer_team(self):
+        return self.organization.get_team(DEV_TEAM_ID)
+
+    def get_user(self, id):
+        return self.github.get_user(id)
+
+    def get_user_by_username(self, username):
+        pass
+
+    def is_core_contributor(self, username):
+        user = self.get_user_by_username(username)
+        return self.developer_team.has_in_members(user)
+
+    def get_pull_request(self, pull_id):
+        return self.github.get_pull(pull_id)
+
+    def add_comment_to_pull_request(self, pull_id, comment):
+        pull = self.get_pull_request(pull_id)
+        pull.create_comment(comment)
+
