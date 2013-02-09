8f899ea30088ec78aa3941600ff6c667191a026b
Eric Steele <EricSteele47@gmail.com>
2013-02-09 17:20:39 +0100

    self is already a Github object.

--- src/plonejenkins.middleware/src/plonejenkins/middleware/plonegithub.py
+++ src/plonejenkins.middleware/src/plonejenkins/middleware/plonegithub.py
@@ -8,14 +8,14 @@ class PloneGithub(Github):
 
     @property
     def organization(self):
-        return self.github.get_organization('plone')
+        return self.get_organization('plone')
 
     @property
     def developer_team(self):
-        return self.organization.get_team(DEV_TEAM_ID)
+        return self.get_team(DEV_TEAM_ID)
 
-    def get_user(self, id):
-        return self.github.get_user(id)
+    # def get_user(self, id):
+    #     return self.get_user(id)
 
     def get_user_by_username(self, username):
         pass
@@ -25,7 +25,7 @@ class PloneGithub(Github):
         return self.developer_team.has_in_members(user)
 
     def get_pull_request(self, pull_id):
-        return self.github.get_pull(pull_id)
+        return self.get_pull(pull_id)
 
     def add_comment_to_pull_request(self, pull_id, comment):
         pull = self.get_pull_request(pull_id)
