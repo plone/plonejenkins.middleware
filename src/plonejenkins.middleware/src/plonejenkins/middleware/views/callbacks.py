c94ae4dc7f6799ac8f4f2e55b7ad43ae13decf25
Ramon Navarro Bosch <ramon.nb@gmail.com>
2013-02-10 11:40:39 +0100

    Callback for jenkins pull request

--- src/plonejenkins.middleware/src/plonejenkins/middleware/views/callbacks.py
+++ src/plonejenkins.middleware/src/plonejenkins/middleware/views/callbacks.py
@@ -5,6 +5,44 @@ from plonejenkins.middleware.security import validatejenkins
 callbackCommit = Service(name='Callback for commits', path='/callback/corecommit',
                     description="Callback for commits jobs on jenkins")
 
+callbackPull = Service(name='Callback for pull requests', path='/callback/corepull',
+                    description="Callback for pull request on jenkins")
+
+
+@callbackPull.post()
+@validatejenkins
+def functionCallbackPull(request):
+    """
+    When jenkins is finished it calls this url
+    {"name":"JobName",
+     "url":"JobUrl",
+     "build":{"number":1,
+        "phase":"STARTED",
+        "status":"FAILED",
+        "url":"job/project/5",
+        "full_url":"http://ci.jenkins.org/job/project/5"
+        "parameters":{"branch":"master"}
+     }
+    }
+
+    We are going to write on the comment
+
+    """
+    answer = request.json_body
+    pull_number = request.GET('pull')
+    pull = request.registry.settings['github'].get_pull(pull_number)
+    jk_job = answer['name']
+    full_url = answer['full_url']
+    if answer['build']['phase'] == 'STARTED':
+        #we just started the build
+        pull.create_comment('I\'m going to test this pull with ' + jk_job + ' you can check it at : ' + full_url + ', good luck!')
+    if answer['build']['phase'] == 'COMPLETED' and answer['build']['status'] == 'SUCCESS':
+        # Great it worked
+        pull.create_comment('I tried your pull request on the ' + jk_job + ' and the tests pass!! Congrats!! I own you a beer!! Share your achievment: ' + full_url)
+    if answer['build']['phase'] == 'COMPLETED' and answer['build']['status'] == 'FAILED':
+        # Oooouu it failed
+        pull.create_comment('I tried your pull request on the ' + jk_job + ' and the tests does not pass!! Oups, maybe is not your fault and the tests where not passing without your pull request!: ' + full_url)
+    # TODO we can create a comment hook
 
 @callbackCommit.post()
 @validatejenkins
@@ -26,7 +64,7 @@ def functionCallbackCommit(request):
 
     """
     #import pdb; pdb.set_trace()
-    answer = request.json_body()
+    answer = request.json_body
     commit_hash = request.GET('commit_hash')
     commit = request.registry.settings['github'].get_commit(commit_hash)
     jk_job = answer['name']
